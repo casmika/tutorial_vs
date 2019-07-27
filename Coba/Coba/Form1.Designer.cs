fc626393b26ef78ae43ab9dab00063a5c65255e6
Casmika Saputra <casmikasaputra@gmail.com>
2019-07-27 20:46:26 +0700

    Skrip VS C# Tutorial 1
    Skrip VS C# Tutorial 1 (Jum'at, 26/07/2019)

---
+++ Coba/Coba/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Coba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TextInput = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Temperatur = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kelembapan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kirim";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // TextInput
+            // 
+            this.TextInput.Location = new System.Drawing.Point(28, 67);
+            this.TextInput.Multiline = true;
+            this.TextInput.Name = "TextInput";
+            this.TextInput.Size = new System.Drawing.Size(181, 179);
+            this.TextInput.TabIndex = 1;
+            this.TextInput.Text = "Apawsd";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(402, 68);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(447, 178);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3"});
+            this.comboBox1.Location = new System.Drawing.Point(66, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "(pilih)";
+            this.comboBox1.Click += new System.EventHandler(this.ComboBox1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "19200"});
+            this.comboBox2.Location = new System.Drawing.Point(263, 17);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "(pilih)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Baudrate";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Temperatur,
+            this.Kelembapan});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 282);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Temperatur
+            // 
+            this.Temperatur.HeaderText = "Temperatur";
+            this.Temperatur.Name = "Temperatur";
+            // 
+            // Kelembapan
+            // 
+            this.Kelembapan.HeaderText = "Kelembapan";
+            this.Kelembapan.Name = "Kelembapan";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(402, 262);
+            this.chart1.Name = "chart1";
+            series3.BorderWidth = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Color = System.Drawing.Color.Red;
+            series3.Legend = "Legend1";
+            series3.MarkerBorderColor = System.Drawing.Color.Red;
+            series3.MarkerBorderWidth = 2;
+            series3.MarkerColor = System.Drawing.Color.White;
+            series3.MarkerSize = 12;
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series3.Name = "T";
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.Blue;
+            series4.Legend = "Legend1";
+            series4.MarkerBorderColor = System.Drawing.Color.Blue;
+            series4.MarkerBorderWidth = 2;
+            series4.MarkerColor = System.Drawing.Color.White;
+            series4.MarkerSize = 12;
+            series4.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Square;
+            series4.Name = "H";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(447, 190);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(774, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Clear All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 486);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TextInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Aplikasi Serial";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TextInput;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Temperatur;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kelembapan;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Coba/Coba/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Coba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailablePort();
+        }
+
+        void getAvailablePort()
+        {
+            comboBox1.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+            comboBox1.Items.Add("Refresh");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(TextInput.Text);
+        }
+
+        private void ComboBox1_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.Text == "Refresh")
+            {
+                getAvailablePort();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Start")
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = int.Parse(comboBox2.Text);
+                serialPort1.ReadTimeout = 1000;
+                serialPort1.WriteTimeout = 1000;
+                serialPort1.DataReceived += SerialPort1_DataReceived;
+                serialPort1.Open();
+
+                button2.Text = "Stop";
+            }
+            else
+            {
+                serialPort1.Close();
+                button2.Text = "Start";
+            }
+        }
+
+        string DataMasuk = "";
+
+        int dataIn;
+        byte[] dataBuf = new byte[10];
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+            try
+            {
+                SerialPort sp = (SerialPort)sender;
+                //DataMasuk = serialPort1.ReadLine();
+                dataIn = serialPort1.ReadByte();
+                this.Invoke(new EventHandler(tampilin2));
+            }
+            catch (TimeoutException)
+            {
+                serialPort1.DiscardInBuffer();
+            }
+        }
+
+        bool sudahKetemuHeader = false;
+        int index = 0;
+        void tampilin2(object sender, EventArgs e)
+        {
+            if(dataIn == 0x7E)
+            {
+                sudahKetemuHeader = true;
+            }
+
+            if(sudahKetemuHeader == true)
+            {
+                dataBuf[index] = (byte)dataIn;
+                index++;
+            }
+            if(index > 6)
+            {
+                index = 0;
+                sudahKetemuHeader = false;
+
+
+                textBox2.Text = "Data1 = " + dataBuf[1].ToString("x") + "\t Data2 = " + dataBuf[2].ToString("x") + "\r\n";
+                // Hasil Kali Data ke-1 dan data ke-2
+                int hasil = dataBuf[1] * dataBuf[2];
+                textBox2.Text += "Hasil Kali = " + hasil.ToString() + "\r\n";
+            }
+        }
+
+        void tampilin(object sender, EventArgs e)
+        {
+            
+            //DATA=25;80
+            char[] splitter = { '=', ';' };
+            string[] dataLine = DataMasuk.Split(splitter);
+
+            textBox2.Text += "Temperature = " + dataLine[1] + " dan Kelembapan = " + dataLine[2];
+            // Temp x Kelembapan
+
+            double ujiDouble;
+            if (double.TryParse(dataLine[1], out ujiDouble) && double.TryParse(dataLine[2], out ujiDouble)) {
+
+                double T = double.Parse(dataLine[1]);
+                double H = double.Parse(dataLine[2]);
+                double Hasil = T * H;
+                textBox2.Text += "\t" + Hasil.ToString();
+            } else
+            {
+                textBox2.Text += "\tError";
+            }
+            textBox2.Text += "\r\n";
+
+            dataGridView1.Rows.Add(dataLine[1], dataLine[2]);
+
+            chart1.Series["T"].Points.Add(double.Parse(dataLine[1]));
+            chart1.Series["H"].Points.Add(double.Parse(dataLine[2]));
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            dataGridView1.Rows.Clear();
+            chart1.Series["T"].Points.Clear();
+            chart1.Series["H"].Points.Clear();
+        }
+    }
+}
